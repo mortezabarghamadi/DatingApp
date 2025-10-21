531f029aad75f2ad0c70deb5e9ab6aafec418323
mortezabarghamadi <mortezabarghamadi5@gmail.com>
2025-10-21 15:15:12 +0330

    New Update

--- DatingApp.Api/Program.cs
+++ DatingApp.Api/Program.cs
@@ -10,7 +10,7 @@ using DatingApp.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllersWithViews();
 
 

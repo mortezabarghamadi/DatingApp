c48ff96510e9235872189cd6dfcbb074ec366b5e
mortezabarghamadi <mortezabarghamadi5@gmail.com>
2026-06-28 23:32:27 +0330

    Add advanced filter

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd3545fe1e09203bb4dff4aedff9f82b399f3565")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b029df33bb2e5928d9bfd5c5f842bd4a36a4689")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
